eeda7e08cfcf283266343ebff7a873a7230c6678
flink-data <flink-data@gmail.com>
2025-02-28 20:03:08 -0800

    class 3

--- src/main/java/flink1/ClickSource.java
+++ src/main/java/flink1/ClickSource.java
@@ -16,8 +16,8 @@ public class ClickSource implements SourceFunction<Event> {
             String url = urls[random.nextInt(urls.length)];
             Long timestamp = Calendar.getInstance().getTimeInMillis();
             ctx.collect(new flink1.Event(user, url, timestamp));
+            Thread.sleep(1000L);
         }
-        Thread.sleep(100L);
     }
 
     @Override
